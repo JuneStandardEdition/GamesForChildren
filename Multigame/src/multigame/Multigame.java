521b1c85dd0ef2f7d8d293a4a0664db3dba85af1
Leeroy Jenkins <llopes.kentin@gmail.com>
2020-03-05 08:53:52 +0100

    [self] Rev 0.1.1 Added : * Initial commit uwu

---
+++ Multigame/src/multigame/Multigame.java
@@ -0,0 +1,18 @@
+package multigame;
+
+/**
+ *
+ * @author Le meilleur des groupes
+ * @desc Application avec interface graphique, regroupant des jeux pour enfant.
+ */
+public class Multigame {
+
+    /**
+     * @param args the command line arguments
+     * @version 0.1.1 : initial commit
+     */
+    public static void main(String[] args) {
+        // TODO
+    }
+
+}
