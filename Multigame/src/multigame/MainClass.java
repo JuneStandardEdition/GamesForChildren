22a03e1117d354ced2efce6a2b03986b2950b6fd
Leeroy Jenkins <llopes.kentin@gmail.com>
2020-03-06 08:44:14 +0100

    0.2.1: added sqlconnection

--- Multigame/src/multigame/MainClass.java
+++ Multigame/src/multigame/MainClass.java
@@ -6,7 +6,7 @@ import gui.GlobalGUI;
  *
  * @author Le meilleur des groupes
  * @desc Application avec interface graphique, regroupant des jeux pour enfant.
- * @version 0.1.9.2
+ * @version 0.2.1: SQLConnection
  */
 public class MainClass {
 
