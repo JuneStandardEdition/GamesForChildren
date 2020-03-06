61ec5fb28190889bffd97ee6ad7207bd84507986
Leeroy Jenkins <llopes.kentin@gmail.com>
2020-03-06 11:04:40 +0100

    0.2.4: Properties n Settings

--- Multigame/src/multigame/MainClass.java
+++ Multigame/src/multigame/MainClass.java
@@ -6,7 +6,7 @@ import gui.GlobalGUI;
  *
  * @author Le meilleur des groupes
  * @desc Application avec interface graphique, regroupant des jeux pour enfant.
- * @version 0.2.3: DESSIN
+ * @version 0.2.4: Properties file
  */
 public class MainClass {
 
