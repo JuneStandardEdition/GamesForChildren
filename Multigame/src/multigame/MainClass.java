ffd9a3ce3d8dfb3454661d2f39f7103f8b120ab4
Leeroy Jenkins <llopes.kentin@gmail.com>
2020-03-10 16:06:04 +0100

    0.3.8: Admin Pane & Menu!

--- Multigame/src/multigame/MainClass.java
+++ Multigame/src/multigame/MainClass.java
@@ -5,7 +5,7 @@ import gui.GlobalGUI;
 /**
  * @author Le meilleur des groupes
  * @desc Application avec interface graphique, regroupant des jeux pour enfant.
- * @version 0.3.7: fix menu
+ * @version 0.3.8: Admin Pane & Menu!
  */
 public class MainClass {
 
